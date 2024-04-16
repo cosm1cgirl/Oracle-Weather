fd2541f8eacb133ff7f7baf8058bed2bc43f0d35
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-04-17 00:31:46 +0200

    Updated index.js
    City updated on search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function updateCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input"); //1st
+  let cityElement = document.querySelector("#city"); //3rd
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
 
+let searchFormElement = document.querySelector("#search-form"); ///2nd
+searchFormElement.addEventListener("submit", updateCity);
